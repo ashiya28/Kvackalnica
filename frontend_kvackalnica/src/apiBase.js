a1609eb8e8667a8235888227c7415fa7387488c8
Anja Molnar <anjamolnar01@gmail.com>
2025-12-23 15:39:56 +0100

    changes

--- frontend_kvackalnica/src/apiBase.js
+++ frontend_kvackalnica/src/apiBase.js
@@ -1,5 +1,1 @@
-const viteEnv =
-  typeof import.meta !== "undefined" && import.meta.env ? import.meta.env : {};
-
-export const API_BASE =
-  viteEnv.VITE_API_URL || process.env.VITE_API_URL || "http://localhost:5000";
+export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
